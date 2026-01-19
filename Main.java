8350882758b808f57a1a77a226432fdac37a98f9
jenifer rocha <jeniferdutra2000@icloud.com>
2026-01-19 20:36:37 -0300

    feat: initial java project structure and hello world

---
+++ Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+    // variavel
+    int minhaIdade = 25;
+
+    var meuNome = "Jenifer";
+    }
+
+    // tipos primitivos
+    // existem diferentes tipos de inteiros
+    // Byte: vai ocupar somente 8 bits na memória => -128 a 127, se for fora desse intervalo vai dar erro... economiza espaco na memoria
+    // Short: 16 bits => -32.768 a 32.767 
+    // Int: 32 bits => -2.147.483.648 a 2.147.483.647
+    // Long: 64 bits => -9.223.372.036.854.775.808 a -9.223.372.036.854.775.807
+
+    // Float: numeros decimais => 32 bits, precisao simples
+    // Double: precisao dupla, tem bastante numeros apos a virgula
+}
