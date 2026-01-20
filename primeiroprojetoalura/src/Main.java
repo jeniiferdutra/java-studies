c9490bfd4b5baff21a8bed6eb5c50258391ef6ae
jenifer rocha <jeniferdutra2000@icloud.com>
2026-01-20 20:41:32 -0300

    feat: implement string manipulation and text formatting

--- primeiroprojetoalura/src/Main.java
+++ primeiroprojetoalura/src/Main.java
@@ -10,6 +10,17 @@ public class Main {
         boolean incluidoPlano = false;
         double notaFilme = 8.1;
 
+        double media = (9.8 + 6.3 + 8.0) / 3;
+        System.out.println("nota média do filme: " + media);
+
+        String sinopse = """ 
+                filme top gun 
+                filme de aventura com gala dos anos 80
+                muito bom!!
+                ano de lançamento
+                """ + ano; // para quebrar linha
+        System.out.println(sinopse);
+
         boolean igual = (7 == 8); //Nesse caso a variável igual ficará com o valor *false*, pois o valor de b não é igual o valor de a.
         boolean diferente = (9 != 6); //A variável diferente ficará com o valor *true*, pois o valor de b é diferente do valor de c.
         boolean maior = (3 > 5); //A variável maior ficará com o valor *false*, pois o valor de b é menor que o valor de a.
@@ -25,6 +36,20 @@ public class Main {
         System.out.println(num); // imprime 6
         System.out.println(resultado); // imprime 5
 
+        // Nomes de constantes devem ser totalmente em letras maiúsculas, separadas por underline.
+        // public static final String NOME_SISTEMA = "JavaApp";
+
+        // Recomenda-se usar espaços em branco para separar operadores, palavras-chave e elementos de controle de fluxo.
+        // Exemplo: if (condicao) {.
+
+        String senha = "12345";
+        if (senha.equals("12345")) {
+            System.out.println("Acesso autorizado!");
+        } else {
+            System.out.println("Senha incorreta.");
+        }
+
+        // o método equals() é utilizado para comparar o conteúdo da variável senha com a String "12345". Se as duas Strings forem iguais, a mensagem "Acesso autorizado!" será impressa
     }
 
 }
