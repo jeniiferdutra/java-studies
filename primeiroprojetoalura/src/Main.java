b0a3222f287457e75bdfa2be3d030073687fd448
jenifer rocha <jeniferdutra2000@icloud.com>
2026-01-20 20:03:02 -0300

    feat: implement movie data assignment and variables

--- primeiroprojetoalura/src/Main.java
+++ primeiroprojetoalura/src/Main.java
@@ -2,8 +2,12 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Alo , Mundoo!");
+        System.out.println("Esse é o Screen Match!");
+        System.out.println("Filme: Top Gun: Maverick"); // sout é o atalho para n precisar escrever System...
+
+        int ano = 2022;
+        System.out.println("ano de lancamento: " + ano);
+        boolean incluidoPlano = false;
+        double notaFilme = 8.1;
     }
 }
