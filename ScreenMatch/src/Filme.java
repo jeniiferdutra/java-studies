dfd5c54fe02ea5154eae2ef4ce9a2734af882b62
jenifer rocha <jeniferdutra2000@icloud.com>
2026-01-27 21:20:26 -0300

    feat: define class structure with attributes

---
+++ ScreenMatch/src/Filme.java
@@ -0,0 +1,8 @@
+public class Filme { // TODO FILME TEM UM NOME
+    String nome;
+    int anoDeLancamento;
+    boolean incluidoNoPlano;
+    double avalicao;
+    int totalDeAvaliacoes;
+    int duracaoEmMinutos;
+}
