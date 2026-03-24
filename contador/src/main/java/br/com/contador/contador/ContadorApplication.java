1e7d165f1776711236d6cb9197d58cf13228e185
jenifer rocha <jeniferdutra2000@icloud.com>
2026-03-23 21:22:15 -0300

    feat: implementa serialização de objetos para arquivo tarefa.json usando ObjectMapper

--- contador/src/main/java/br/com/contador/contador/ContadorApplication.java
+++ contador/src/main/java/br/com/contador/contador/ContadorApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import tools.jackson.databind.ObjectMapper;
+import java.io.File;
 import java.util.Scanner;
 
 @SpringBootApplication
@@ -22,5 +24,10 @@ public class ContadorApplication implements CommandLineRunner {
 		for (int i = 0; i <= numero; i++) {
 			System.out.println(i);
 		}
+
+		Tarefa tarefa = new Tarefa("Trabalhar", true, "Jenifer");
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.writeValue(new File("tarefa.json"), tarefa);
+		System.out.println("Arquivo tarefa.json gerado com sucesso!");
 	}
 }
