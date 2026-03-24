4384a91def491df912d49fa6544bd6a605b21d03
jenifer rocha <jeniferdutra2000@icloud.com>
2026-03-24 19:44:12 -0300

    feat: modela DTO de temporadas com records e mapeamento de listas de episódios

--- screenmatch02/src/main/java/br/com/alura/screenmatch02/Screenmatch02Application.java
+++ screenmatch02/src/main/java/br/com/alura/screenmatch02/Screenmatch02Application.java
@@ -2,11 +2,15 @@ package br.com.alura.screenmatch02;
 
 import br.com.alura.screenmatch02.model.DadosEpisodio;
 import br.com.alura.screenmatch02.model.DadosSerie;
+import br.com.alura.screenmatch02.model.DadosTemporada;
 import br.com.alura.screenmatch02.service.ConsumoAPI;
 import br.com.alura.screenmatch02.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 // Fazer aplicacao de linha de comando
@@ -29,5 +33,14 @@ public class Screenmatch02Application implements CommandLineRunner {
 		json = consumoAPI.obterDados("https://omdbapi.com/?t=gilmore+girls&Season=1&episode=2&apikey=e5be24ea");
 		DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
 		System.out.println("Dados do episódio: " + dadosEpisodio);
+
+		List<DadosTemporada> temporadas = new ArrayList<>();
+
+		for (int i = 1; i <= dados.totalTemporadas(); i++) {
+			json = consumoAPI.obterDados("https://omdbapi.com/?t=gilmore+girls&Season=" + i + "&apikey=e5be24ea");
+			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+			temporadas.add(dadosTemporada);
+		}
+		temporadas.forEach(System.out::println);
 	}
 }
