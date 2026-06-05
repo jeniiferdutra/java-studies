1ee8c34c29ddef3db387cba8ae9d4af28d1e9ff2
jenifer rocha <jeniferdutra2000@icloud.com>
2026-06-05 15:48:19 -0300

    feat: adiciona consultas com múltiplos critérios utilizando operadores lógicos no repositório

--- ScreenMatchApplication/src/main/java/br/com/alura/ScreenMatchApplication/main/Principal.java
+++ ScreenMatchApplication/src/main/java/br/com/alura/ScreenMatchApplication/main/Principal.java
@@ -44,6 +44,7 @@ public class Principal {
                     2 - Buscar episódios
                     3 - Listar séries buscadas
                     4 - Buscar série por título
+                    5 - Buscar séries por ator
                     0 - Sair
                     """;
 
@@ -63,7 +64,10 @@ public class Principal {
                     break;
                 case 4:
                     buscarSeriePorTitulo();
-                    break; 
+                    break;
+                case 5:
+                    buscarSeriesPorAtor();
+                    break;
                 case 0:
                     System.out.println("Saindo...");
                     break;
@@ -153,5 +157,16 @@ public class Principal {
         } else {
             System.out.println("Série nao encontrada");
         }
+    }
+
+    private void buscarSeriesPorAtor() {
+        System.out.println("Qual o nome para busca:");
+        var nomeAtor = leitura.nextLine();
+        System.out.println("Avaliaçoes a partir de qual valor: ");
+        var avaliacao = leitura.nextDouble();
+        List<Serie> seriesEncontradas = repositorio.findByAtoresContainingIgnoreCaseAndAvaliacaoGreaterThanEqual(nomeAtor, avaliacao);
+        System.out.println("Séries em que " + nomeAtor + " trabalhou:");
+        seriesEncontradas.forEach(s ->
+                System.out.println(s.getTitulo() + " avaliaçao: " + s.getAvaliacao()));
     }
 }
--- ScreenMatchApplication/src/main/java/br/com/alura/ScreenMatchApplication/repository/SerieRepository.java
+++ ScreenMatchApplication/src/main/java/br/com/alura/ScreenMatchApplication/repository/SerieRepository.java
@@ -5,9 +5,13 @@ package br.com.alura.ScreenMatchApplication.repository;
 import br.com.alura.ScreenMatchApplication.model.Serie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface SerieRepository extends JpaRepository<Serie, Long> {
     //Buscar por trecho do título (pode ser que ache ou nao)
     Optional<Serie> findByTituloContainingIgnoreCase(String nomeSerie);
+
+    // Buscar por ator e add mais um critério que é avaliaçao (maior ou igual a 8.8)
+    List<Serie> findByAtoresContainingIgnoreCaseAndAvaliacaoGreaterThanEqual(String nomeAtor, Double avaliacao);
 }
