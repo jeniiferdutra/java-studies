0d4878ea32be121df414f99eb846e9298a94a65e
jenifer rocha <jeniferdutra2000@icloud.com>
2026-06-18 00:08:53 -0300

    feat: cria endpoint para listar todas as séries no navegador em formato JSON

--- ScreenMatchApplication/src/main/java/br/com/alura/ScreenMatchApplication/controller/SerieController.java
+++ ScreenMatchApplication/src/main/java/br/com/alura/ScreenMatchApplication/controller/SerieController.java
@@ -1,14 +1,22 @@
 package br.com.alura.ScreenMatchApplication.controller;
 
+import br.com.alura.ScreenMatchApplication.model.Serie;
+import br.com.alura.ScreenMatchApplication.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class SerieController {
 
+    @Autowired
+    private SerieRepository repository;
+
     // Método vai responder a requisições do tipo HTTP GET
     @GetMapping("/series")
-    public String obterSeries() {
-        return "Aqui vão ser listadas as séries";
+    public List<Serie> obterSeries() {
+        return repository.findAll();
     }
 }
