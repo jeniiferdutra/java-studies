3787dca58620211eeab78606a36dc8e0f6b02931
jenifer rocha <jeniferdutra2000@icloud.com>
2026-06-17 23:19:11 -0300

    build: adiciona dependência do Spring Web

--- ScreenMatchApplication/src/main/java/br/com/alura/ScreenMatchApplication/ScreenMatchApplication.java
+++ ScreenMatchApplication/src/main/java/br/com/alura/ScreenMatchApplication/ScreenMatchApplication.java
@@ -8,18 +8,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScreenMatchApplication implements CommandLineRunner {
+public class ScreenMatchApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenMatchApplication.class, args);
 	}
 
-	@Autowired
-	private SerieRepository repositorio;
-
-	@Override
-	public void run(String... args) throws Exception {
-		Principal principal = new Principal(repositorio);
-		principal.exibeMenu();
-	}
 }
